cb7f641d768969ef809d23adaf2234c3509298cf
kshiprant <kshiprant19@gmail.com>
2026-03-06 04:09:08 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,70 +1,16 @@
 import { auth } from "./firebase.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import {
-
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-signOut
-
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-
-window.register = function () {
-
-const email = document.getElementById("email").value;
-
-const password = document.getElementById("password").value;
-
-createUserWithEmailAndPassword(auth, email, password)
-
-.then((userCredential) => {
-
-alert("Account Created");
-
-window.location.href = "/home.html";
-
-})
-
-.catch((error) => {
-
-alert(error.message);
-
-});
-
+window.logout = async function logout(){
+  await signOut(auth);
+  window.location.href = "/index.html";
 };
 
+// If a page has <body data-protected="1"> then require login
+const protectedPage = document.body?.dataset?.protected === "1";
 
-window.login = function () {
-
-const email = document.getElementById("email").value;
-
-const password = document.getElementById("password").value;
-
-signInWithEmailAndPassword(auth, email, password)
-
-.then((userCredential) => {
-
-window.location.href = "/home.html";
-
-})
-
-.catch((error) => {
-
-alert(error.message);
-
-});
-
-};
-
-
-window.logout = function () {
-
-signOut(auth)
-
-.then(() => {
-
-window.location.href = "/";
-
-});
-
-};
+if (protectedPage) {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) window.location.href = "/index.html";
+  });
+}
