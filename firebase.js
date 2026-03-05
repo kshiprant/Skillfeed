5c75d3593359b94b1b948d1bca8640f43c59a709
kshiprant <kshiprant19@gmail.com>
2026-03-06 00:17:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,48 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { getAuth,
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-signOut }
-
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import { getFirestore,
-collection,
-addDoc,
-getDocs,
-query,
-where }
-
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyCfsQJHExYNMyUb4eNFqdCQxW50qPtmrok",
-authDomain: "skillfeed2026.firebaseapp.com",
-projectId: "skillfeed2026",
-storageBucket: "skillfeed2026.firebasestorage.app",
-messagingSenderId: "516697457596",
-appId: "1:516697457596:web:38edcd623662153ffbe1b9"
-
+ apiKey: "AIzaSyCfsQJHExYNMyUb4eNFqdCQxW50qPtmrok",
+ authDomain: "skillfeed2026.firebaseapp.com",
+ projectId: "skillfeed2026",
+ storageBucket: "skillfeed2026.firebasestorage.app",
+ messagingSenderId: "516697457596",
+ appId: "1:516697457596:web:38edcd623662153ffbe1b9",
+ measurementId: "G-TC9M770VGL"
 };
 
 const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export {
-
-auth,
-db,
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-signOut,
-collection,
-addDoc,
-getDocs,
-query,
-where
-
-};
+export const auth = getAuth(app);
