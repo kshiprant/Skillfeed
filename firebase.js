294b99d17576826bec6f837f728654b79e21bb95
kshiprant <kshiprant19@gmail.com>
2026-03-06 04:08:35 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,35 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-
 const firebaseConfig = {
-
-apiKey: "AIzaSyCfsQJHExYNMyUb4eNFqdCQxW50qPtmrok",
-
-authDomain: "skillfeed2026.firebaseapp.com",
-
-projectId: "skillfeed2026",
-
-storageBucket: "skillfeed2026.firebasestorage.app",
-
-messagingSenderId: "516697457596",
-
-appId: "1:516697457596:web:38edcd623662153ffbe1b9",
-
-measurementId: "G-TC9M770VGL"
-
+  apiKey: "AIzaSyCfsQJHExYNMyUb4eNFqdCQxW50qPtmrok",
+  authDomain: "skillfeed2026.firebaseapp.com",
+  projectId: "skillfeed2026",
+  storageBucket: "skillfeed2026.firebasestorage.app",
+  messagingSenderId: "516697457596",
+  appId: "1:516697457596:web:38edcd623662153ffbe1b9",
+  measurementId: "G-TC9M770VGL"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-
-const db = getFirestore(app);
-
-
-// Export both so other files can use them
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
