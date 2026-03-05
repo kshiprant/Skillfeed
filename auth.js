2b0c75ce9461eb49a1a3a9bb288d405d234ba4a8
kshiprant <kshiprant19@gmail.com>
2026-03-06 00:44:49 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,23 +1,30 @@
 import { auth } from "./firebase.js";
 
-import { 
+import {
+
 createUserWithEmailAndPassword,
-signInWithEmailAndPassword
+signInWithEmailAndPassword,
+signOut
+
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 
 window.register = function () {
 
 const email = document.getElementById("email").value;
+
 const password = document.getElementById("password").value;
 
 createUserWithEmailAndPassword(auth, email, password)
+
 .then((userCredential) => {
 
-const user = userCredential.user;
+alert("Account Created");
 
-alert("Registration Successful: " + user.email);
+window.location.href = "/home.html";
 
 })
+
 .catch((error) => {
 
 alert(error.message);
@@ -26,22 +33,37 @@ alert(error.message);
 
 };
 
+
 window.login = function () {
 
 const email = document.getElementById("email").value;
+
 const password = document.getElementById("password").value;
 
 signInWithEmailAndPassword(auth, email, password)
+
 .then((userCredential) => {
 
-const user = userCredential.user;
-
-alert("Login Successful: " + user.email);
+window.location.href = "/home.html";
 
 })
+
 .catch((error) => {
 
 alert(error.message);
+
+});
+
+};
+
+
+window.logout = function () {
+
+signOut(auth)
+
+.then(() => {
+
+window.location.href = "/";
 
 });
 
